41658aa17c8a2d6f6f3433d67c4b7f2cf43b92c7
pawan siwakoti <pawankumarsiwakoti@pawans-iMac.local>
2017-06-02 15:40:49 +1000

    first commit

---
+++ Trip Gallery/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Trip Gallery
+//
+//  Created by pawan kumar siwakoti on 2/6/17.
+//  Copyright © 2017 pawan kumar siwakoti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
